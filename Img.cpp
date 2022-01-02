66eb318de284d0b2f4daccb338001e884aaf7bb3
Thanatos <jepthanatos@gmail.com>
2022-01-02 21:46:26 +0100

    Version 0.2 - Fix style errors
    Style errors fixed in both files.

--- Img.cpp
+++ Img.cpp
@@ -1,13 +1,12 @@
 //============================================================================
 // Name        : Img.cpp
-// Author      :
-// Version     :
+// Author      : Jorge
+// Version     : 0.2
 // Copyright   :
 // Description : File to wrap CImg library.
 //============================================================================
 
 #include "Img.h"
-
 
 //==============================================================================
 /* Img */
@@ -27,73 +26,74 @@ Img::Img(int w, int h, std::string title) :
  * Function to get the height.
  */
 int Img::getHeight() const {
-    return img.height();
+	return img.height();
 }
 
 /*------------------------------------------------------------------------------
  * Function to get the width.
  */
 int Img::getWidth() const {
-    return img.width();
+	return img.width();
 }
 
 /*------------------------------------------------------------------------------
  * Function to know if the image is closed.
  */
 bool Img::isClosed() const {
-    return disp.is_closed();
+	return disp.is_closed();
 }
 
 /*------------------------------------------------------------------------------
  * Procedure to set the title of the window.
  */
-void Img::setTitle(const char* title) {
-    disp.set_title(title);
+void Img::setTitle(const char *title) {
+	disp.set_title(title);
 }
 
 /*------------------------------------------------------------------------------
  * Procedure to draw a point.
  */
-void Img::drawPoint(int col, int row, float* color) {
-    img.draw_point(col, row, color);
+void Img::drawPoint(int col, int row, float *color) {
+	img.draw_point(col, row, color);
 }
 
 /*------------------------------------------------------------------------------
  * Procedure to draw a circle.
  */
-void Img::drawCircle(int col, int row, float* color)
-{
+void Img::drawCircle(int col, int row, float *color) {
 	img.draw_circle(col, row, 500, color, 0.5f);
 }
 
 /*------------------------------------------------------------------------------
  * Procedure to draw a line.
  */
-void Img::drawLine(int x0, int y0, int x1, int y1, float* color)
-{
+void Img::drawLine(int x0, int y0, int x1, int y1, float *color) {
 	img.draw_line(x0, y0, x1, y1, color);
 }
 
 /*------------------------------------------------------------------------------
  * Procedure to draw a rectangle.
  */
-void Img::drawRectangle(const int x0, const int y0, const int x1, const int y1, float* color, const float opacity){
-	img.draw_rectangle(x0,y0,x1,y1,color,opacity);
+void Img::drawRectangle
+	(const int x0, const int y0, const int x1, const int y1,
+	 float *color, const float opacity)
+{
+	img.draw_rectangle(x0, y0, x1, y1, color, opacity);
 }
 
 /*------------------------------------------------------------------------------
  * Procedure to display the image.
  */
 void Img::display() {
-    disp.display(img);
+	disp.display(img);
 }
 
 /*------------------------------------------------------------------------------
  * Function to know if the left mouse button has been clicked.
  * Returns true (false) if the left mouse button has (not) been clicked.
  */
-bool Img::mouseClick(){
-	return disp.button()&1;
+bool Img::mouseClick() {
+	return disp.button() & 1;
 }
 
 //==============================================================================
--- Img.h
+++ Img.h
@@ -1,7 +1,7 @@
 //============================================================================
 // Name        : Img.h
 // Author      : Jorge
-// Version     : 0.1
+// Version     : 0.2
 // Copyright   :
 // Description : File to wrap CImg library.
 //============================================================================
@@ -26,35 +26,37 @@ class Img {
 public:
 
 	// Constructor.
-    Img(int w, int h, std::string title);
+	Img(int w, int h, std::string title);
 
-    // Functions to get the height and width of the image.
-    int getHeight() const;
-    int getWidth() const;
+	// Functions to get the height and width of the image.
+	int getHeight() const;
+	int getWidth() const;
 
-    // Function to know if the image is closed.
-    bool isClosed() const;
+	// Function to know if the image is closed.
+	bool isClosed() const;
 
-    // Procedure to set the title of the window.
-    void setTitle(const char* title);
+	// Procedure to set the title of the window.
+	void setTitle(const char *title);
 
-    // Draw procedures.
-    void drawPoint(int col, int row, float* color);
-    void drawCircle(int col, int row, float* color);
-    void drawLine(int x0, int y0, int x1, int y1, float* color);
-    void drawRectangle(const int x0, const int y0, const int x1, const int y1, float* color, const float opacity=1);
+	// Draw procedures.
+	void drawPoint(int col, int row, float *color);
+	void drawCircle(int col, int row, float *color);
+	void drawLine(int x0, int y0, int x1, int y1, float *color);
+	void drawRectangle
+		(const int x0, const int y0, const int x1, const int y1,
+		 float *color, const float opacity = 1);
 
-    // Procedure to display the image.
-    void display();
+	// Procedure to display the image.
+	void display();
 
-    // Function to know if the left mouse button has been clicked.
-    // Returns true (false) if the left mouse button has (not) been clicked.
-    bool mouseClick();
+	// Function to know if the left mouse button has been clicked.
+	// Returns true (false) if the left mouse button has (not) been clicked.
+	bool mouseClick();
 
 private:
 
-    cimg_library::CImg<float> img;
-    cimg_library::CImgDisplay disp;
+	cimg_library::CImg<float> img;
+	cimg_library::CImgDisplay disp;
 
 };
 
